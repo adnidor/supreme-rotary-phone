cefb7af29d48217b3eb29923eac18dd85beed59d
Yannik <git@adnidor.de>
2016-05-01 13:55:31 +0200

    added root check

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [[ $(whoami) != "root" ]]
+then
+echo not root, exiting
+exit 1
+fi
+
 basepath="$(dirname "$(readlink -f "$0")")"
 "$basepath/dynamicbindconfig.sh"
 "$basepath/dynamicdhcphosts.sh"
