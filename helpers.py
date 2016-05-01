a40a074b6421b71c29afdb413453b9a64dad0d30
Yannik <me@adnidor.de>
2016-05-01 23:06:21 +0200

    added helpers

---
+++ helpers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#coding=utf-8
+import mysql.connector as ms
+from importlib.machinery import SourceFileLoader
+
+server_config = SourceFileLoader("server_config", "/etc/networkmanagement/server_config.py").load_module()
+
+
+DOMAIN=server_config.domain
+
+def get_fqdn(identifier):
+    db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
+    cur = db.cursor()
+
+    cur.execute("SELECT hostname,context FROM devices WHERE identifier='"+identifier+"'")
+    devices = cur.fetchall()
+    devicename = devices[0][0]
+    context = devices[0][1]
+    if context == "root":
+        contextstr = "."
+    else:
+        contextstr = "."+context+"."
+    return devicename+contextstr+DOMAIN
