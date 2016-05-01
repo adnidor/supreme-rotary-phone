47ed6c50008ca7509f51128a9992797c1084b2b4
Yannik <me@adnidor.de>
2016-05-02 00:03:31 +0200

    updated listdevices.py to use helper function

--- listdevices.py
+++ listdevices.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #coding=utf-8
 import mysql.connector as ms
+from helpers import get_fqdn
 from importlib.machinery import SourceFileLoader
 
 server_config = SourceFileLoader("server_config", "/etc/networkmanagement/server_config.py").load_module()
@@ -11,14 +12,9 @@ DOMAIN=server_config.domain
 db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
 cur = db.cursor()
 
-cur.execute("SELECT hostname,context FROM devices")
+cur.execute("SELECT identifier FROM devices")
 contexts = cur.fetchall()
 
 for context in contexts:
-    devicename = context[0]
-    context = context[1]
-    if context == "root":
-        contextstr = "."
-    else:
-        contextstr = "."+context+"."
-    print(devicename+contextstr+DOMAIN)
+    identifier = context[0]
+    print(get_fqdn(identifier))
