525cba988607ed3641ac8932137bbc28a564b4dc
Yannik <me@adnidor.de>
2016-05-05 23:28:38 +0200

    improvements

--- dynamic/openwrt.sh
+++ dynamic/openwrt.sh
@@ -6,9 +6,10 @@ do
 	"$basepath/generateowrtwireless.py" "$ap" > "$basepath/tmp/$ap.wireless"
 	"$basepath/generateowrtnetwork.py" "$ap" > "$basepath/tmp/$ap.network"
     ip="$("$basepath/getapip.py" "$ap")"
-    scp "$basepath/tmp/$ap.wireless" root@$ip:/etc/config/wireless
-    scp "$basepath/tmp/$ap.network" root@$ip:/etc/config/network
+    scp -q "$basepath/tmp/$ap.wireless" root@$ip:/etc/config/wireless
+    scp -q "$basepath/tmp/$ap.network" root@$ip:/etc/config/network
     ssh root@$ip 'uci commit network; uci commit wireless; wifi'
-    rm "$basepath/tmp/$ap.*"
+    rm "$basepath/tmp/$ap.network"
+    rm "$basepath/tmp/$ap.wireless"
 done
 
