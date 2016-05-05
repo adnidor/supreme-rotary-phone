080d48624321cd69c4949dce324fe42b94cf181d
Yannik <me@adnidor.de>
2016-05-05 21:28:22 +0200

    added openwrt scripts

---
+++ generateowrtnetwork.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import mysql.connector as ms
+import ipaddress as ipa
+import socket, struct
+import sys
+from importlib.machinery import SourceFileLoader
+
+if len(sys.argv) != 2:
+    print("usage: "+sys.argv[0]+" <ap>")
+    exit(1)
+ap = sys.argv[1] #erster Parameter
+
+server_config = SourceFileLoader("server_config", "/etc/networkmanagement/server_config.py").load_module()
+
+
+print("#DO NOT EDIT - This file was generated automatically from an MySQL-Database")
+
+db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
+cur = db.cursor()
+
+cur.execute("SELECT id, vlans, mvlan FROM aps WHERE name = '"+ap+"'")
+result = cur.fetchall()[0]
+apid = result[0]
+vlans = result[1].split(",")
+mvlan = result[2]
+
+print("config interface 'loopback'")
+print("    option ifname 'lo'")
+print("    option proto 'static'")
+print("    option ipaddr '127.0.0.1'")
+print("    option netmask '255.0.0.0'")
+print()
+
+for vlan in vlans:
+    print("config interface 'vlan"+vlan+"'")
+    print("    option ifname 'eth0."+vlan+"'")
+    print("    option type 'bridge'")
+    if vlan == str(mvlan):
+        print("    option proto 'dhcp'")
+    else:
+        print("    option proto 'none'")
+    print()
+
+
+try:
+    manual = open("/etc/networkmanagement/"+ap+".network.manual", "r")
+    print(manual.read())
+except IOError:
+    print()
---
+++ generateowrtwireless.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import mysql.connector as ms
+import ipaddress as ipa
+import socket, struct
+import sys
+from importlib.machinery import SourceFileLoader
+
+if len(sys.argv) != 2:
+    print("usage: "+sys.argv[0]+" <ap>")
+    exit(1)
+ap = sys.argv[1] #erster Parameter
+
+server_config = SourceFileLoader("server_config", "/etc/networkmanagement/server_config.py").load_module()
+
+
+print("#DO NOT EDIT - This file was generated automatically from an MySQL-Database")
+
+db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
+cur = db.cursor()
+
+cur.execute("SELECT id, channel, radiussecret, radiusserver FROM aps WHERE name = '"+ap+"'")
+result = cur.fetchall()[0]
+apid = result[0]
+channel = result[1]
+radiussecret = result[2]
+radiusserver = result[3]
+
+print("config wifi-device 'radio0'")
+print("    option type 'mac80211'")
+print("    option hwmode '11g'")
+print("    option htmode 'HT40'")
+print("    option country 'DE'")
+print("    option channel '"+str(channel)+"'")
+print()
+
+cur.execute("SELECT aps,ssid, authmethod, vlan, hidden, passphrase, mode FROM wifis")
+
+wifis = cur.fetchall()
+
+for wifi in wifis:
+    aps = wifi[0].split(",")
+    if str(apid) not in aps:
+        continue
+    ssid = wifi[1]
+    authmethod = wifi[2]
+    vlan = wifi[3]
+    hidden = True if wifi[4] == 1 else False
+    passphrase = wifi[5]
+    mode = wifi[6]
+    print("config wifi-iface")
+    print("    option device 'radio0'")
+    print("    option mode '"+mode+"'")
+    print("    option ssid '"+ssid+"'")
+    if hidden:
+        print("    option hidden '1'")
+    if vlan > 0:
+        print("    option network 'vlan"+str(vlan)+"'")
+    if authmethod == 'none':
+        print("    option encryption 'none'")
+    elif authmethod == "radius":
+        print("    option encryption 'wpa2'")
+        print("    option auth_server '"+radiusserver+"'")
+        print("    option auth_secret '"+radiussecret+"'")
+    elif authmethod == "passphrase":
+        print("    option encryption 'psk2'")
+        print("    option key '"+passphrase+"'")
+    print()
