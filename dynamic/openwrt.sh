9bd2df37c50322e895860656f06005d97de702bc
Yannik <me@adnidor.de>
2016-05-05 23:14:11 +0200

    push openwrt updates

--- dynamic/openwrt.sh
+++ dynamic/openwrt.sh
@@ -3,7 +3,12 @@ basepath="$(dirname "$(dirname "$(readlink -f "$0")")")"
 mkdir -p "$basepath/tmp"
 for ap in $("$basepath/listaps.py")
 do
-	"$basepath/generateowrtwireless.py" $ap > "$basepath/tmp/$ap.wireless"
-	"$basepath/generateowrtnetwork.py" $ap > "$basepath/tmp/$ap.network"
+	"$basepath/generateowrtwireless.py" "$ap" > "$basepath/tmp/$ap.wireless"
+	"$basepath/generateowrtnetwork.py" "$ap" > "$basepath/tmp/$ap.network"
+    ip="$("$basepath/getapip.py" "$ap")"
+    scp "$basepath/tmp/$ap.wireless" root@$ip:/etc/config/wireless
+    scp "$basepath/tmp/$ap.network" root@$ip:/etc/config/network
+    ssh root@$ip 'uci commit network; uci commit wireless; wifi'
+    rm "$basepath/tmp/$ap.*"
 done
 
