40fc81ef1a58c70120f8a6eafc823d5f1406bd45
Yannik <me@adnidor.de>
2016-05-05 22:36:34 +0200

    use tmp dir

--- dynamic/openwrt.sh
+++ dynamic/openwrt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 basepath="$(dirname "$(dirname "$(readlink -f "$0")")")"
+mkdir -p "$basepath/tmp"
 for ap in $("$basepath/listaps.py")
 do
-	"$basepath/generateowrtwireless.py" $ap > "$basepath/$ap.wireless"
-	"$basepath/generateowrtnetwork.py" $ap > "$basepath/$ap.network"
+	"$basepath/generateowrtwireless.py" $ap > "$basepath/tmp/$ap.wireless"
+	"$basepath/generateowrtnetwork.py" $ap > "$basepath/tmp/$ap.network"
 done
 
