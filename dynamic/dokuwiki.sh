5f279ca1c8977bc152b4bfe117379efa86dca8d0
Yannik <me@adnidor.de>
2016-05-06 17:25:15 +0200

    added wifi -> dokuwiki script

--- dynamic/dokuwiki.sh
+++ dynamic/dokuwiki.sh
@@ -3,3 +3,4 @@ basepath="$(dirname "$(dirname "$(readlink -f "$0")")")"
 "$basepath/generatedokuwikidevices.py" > /var/www/dokuwiki/data/pages/network/devices_generated.txt
 "$basepath/generatedokuwikicontexts.py" > /var/www/dokuwiki/data/pages/network/contexts_generated.txt
 "$basepath/generatedokuwikicnames.py" > /var/www/dokuwiki/data/pages/network/cnames_generated.txt
+"$basepath/generatedokuwikiwifis.py" > /var/www/dokuwiki/data/pages/network/wifis_generated.txt
---
+++ generatedokuwikiwifis.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#coding=utf-8
+import mysql.connector as ms
+from importlib.machinery import SourceFileLoader
+
+server_config = SourceFileLoader("server_config", "/etc/networkmanagement/server_config.py").load_module()
+
+
+
+print("====== WLAN-Netzwerke ======")
+print("DO NOT EDIT - This file is generated automatically")
+
+db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
+cur = db.cursor()
+
+cur.execute("SELECT id, name FROM aps")
+
+aps = cur.fetchall()
+
+cur.execute("SELECT aps,ssid, authmethod, vlan, hidden, mode FROM wifis")
+wifis = cur.fetchall()
+
+for ap in aps:
+    id = ap[0]
+    name = ap[1]
+    print("====="+name+"=====")
+    print("^SSID ^VLAN ^Verschlüsselung ^Versteckt? ^Modus ^")
+    for wifi in wifis:
+        if str(id) not in wifi[0].split(","):
+            continue
+        ssid = wifi[1]
+        encryption = wifi[2]
+        vlan = wifi[3] if wifi[3] > 0 else "-"
+        hidden = "Ja" if wifi[4] == 1 else "Nein"
+        mode = wifi[5]
+        print("|"+ssid+"|"+str(vlan)+"|"+encryption+"|"+hidden+"|"+mode+"|")
+
