fec092a19acdd29ff2304e119e5d6bf538875865
Yannik <me@adnidor.de>
2016-05-09 00:38:01 +0200

    minor changes

--- generateconfig/generateansibleinventory.py
+++ generateconfig/generateansibleinventory.py
@@ -12,15 +12,13 @@ from helpers import get_fqdn
 db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
 cur = db.cursor()
 
-cur.execute("SELECT number, name, os, platform FROM devicetypes")
+cur.execute("SELECT number, name FROM devicetypes")
 
 devicetypes = cur.fetchall()
 
 for type in devicetypes:
     number = type[0]
     name = type[1]
-    os = type[2]
-    platform = type[3]
     cur.execute("SELECT identifier FROM devices WHERE devicetype="+str(number)+" ORDER BY INET_ATON(ip)")
     results = cur.fetchall()
     if len(results) is 0:
