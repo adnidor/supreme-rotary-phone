5991b89d7a6ced5b2a944c1267258d05545db3ad
Yannik <me@adnidor.de>
2016-05-28 03:06:54 +0200

    bugfix

--- generateconfig/generatedhcphosts.py
+++ generateconfig/generatedhcphosts.py
@@ -10,7 +10,7 @@ print("#DO NOT EDIT - This file was generated automatically from an MySQL-Databa
 
 db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
 cur = db.cursor()
-cur.execute("SELECT identifier, ip, contexts.description, devices.description, hostname FROM devices JOIN contexts ON devices.context = contexts.name WHERE devices.type='dhcp' ORDER BY INET_ATON(ip)")
+cur.execute("SELECT identifier, ip, contexts.description, devices.description, hostname FROM devices JOIN contexts ON devices.context = contexts.id WHERE devices.type='dhcp' ORDER BY INET_ATON(ip)")
 
 for row in cur.fetchall():
     print(row[0]+","+row[1]+" #"+row[3]+" ("+row[4]+") von "+row[2])
