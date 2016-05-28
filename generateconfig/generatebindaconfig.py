ad9bbced4d5ccee6868e65873daf5cac43ce0443
Yannik <me@adnidor.de>
2016-05-28 03:04:45 +0200

    bugfix

--- generateconfig/generatebindaconfig.py
+++ generateconfig/generatebindaconfig.py
@@ -33,7 +33,7 @@ def print_header(suffix):
     print("                        1h )            ; Negative caching TTL of 1 day")
     print("@       IN      NS      ns.intern.yannikenss.de.")
 
-cur.execute("SELECT ip, hostname, description, identifier, altname FROM devices WHERE context='"+context+"' ORDER BY INET_ATON(ip)")
+cur.execute("SELECT ip, hostname, description, identifier, altname FROM devices JOIN contexts ON contexts.id = devices.context WHERE contexts.name='"+context+"' ORDER BY INET_ATON(ip)")
 
 if context == "root":
     prefix = ""
