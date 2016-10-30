26246bc4a8e72feeded1e29409e68f99a37a87f9
Yannik <me@adnidor.de>
2016-10-30 20:24:08 +0100

    refactored checkwifiaccess.py

--- check_access/checkwifiaccess.py
+++ check_access/checkwifiaccess.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import mysql.connector as ms
+#This script doesn't work
 import ipaddress as ipa
 import socket, struct
 import sys,os
@@ -16,6 +16,7 @@ syslog.openlog("wifi-cert-check")
 if len(sys.argv) != 5:
     print("usage: "+sys.argv[0]+" <ca-file> <cert-file> <client-ip> <supplicant-mac>")
     exit(1)
+
 cafile = sys.argv[1]
 certfile = sys.argv[2]
 clientip = sys.argv[3]
@@ -26,9 +27,6 @@ verify_opensslcommand = "openssl verify -CApath '%s' '%s'"%(cafile,certfile)
 
 common_name = check_output(cn_opensslcommand,shell=True,universal_newlines=True).strip()
 syslog.syslog("CN: "+common_name)
-
-db = ms.connect(host=server_config.host, user=server_config.user, passwd=server_config.passwd, db=server_config.db)
-cur = db.cursor()
 
 device = helpers.get_device_from_fqdn(common_name)
 if device is None:
@@ -42,7 +40,7 @@ if device.connection != "wifi":
     exit(1)
 
 try:
-    exit(call(verify_opensslcommand,shell=True))
+    exit(call(verify_opensslcommand))
 except Exception as test:
-    syslog.syslog("Fehler mit openssl: "+test.output)
+    syslog.syslog("Problem with openssl: "+test.output)
     exit(1)
